3a553562269bfa256e032438b58dc71044eb91ad
Vahigoku <106706476+Vahigoku@users.noreply.github.com>
2024-10-20 13:46:28 +0200

    ee

---
+++ ToTheShitter/Assets/Scripts/AgingAger.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgingAger : MonoBehaviour
+{
+  
+    public int currentAge;
+    public int maxAge = 10 ;
+    public void Start()
+    {
+        currentAge += 1;
+        Debug.Log(currentAge);
+        currentAge = 0;
+        Debug.Log("Age is set to 0, restarting...");
+        Aging();
+    }
+    public void Aging()
+    {
+        for(int i = 0; maxAge > currentAge; i ++)
+        {
+            currentAge += 1;
+            Debug.Log("New ages:");
+            Debug.Log(currentAge);
+        }
+    }
+}
